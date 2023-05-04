74f13718c9c16565fa738916fc1c8b7d9582c72a
gc-bianchi <72824902+gc-bianchi@users.noreply.github.com>
2023-05-04 17:48:46 -0600

    updated working function prompting the user for inputs, updated read me

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# elevator
-
+# Elevator project instructions
 - The result should be an executable, or script, that can be run with the following inputs and generate the following outputs.
 - Inputs: [list of floors to visit] (e.g elevator start=12 floor=2,9,1,32)
 - Outputs: [total travel time, floors visited in order] (e.g 560 12,2,9,1,32)
@@ -8,11 +7,11 @@
 # Assumptions
 - Max number of floors is 163, which is the number of floors in the tallest building in the world currently (Burj Khalifa)
 - Base floor is 0
-- Start floor must be an integer greater than 0, less than or equal to 163
+- Start floor must be an integer greater or equal to 0, less than or equal to 163
 - List of floors to visit must have at least one value
 - List of floors must all be integers greater than 0
 - List of floors cannot have a value greater than max number of floors
 
 # Features that weren’t implemented
-- I wanted to prompt the user to set the max number of floors, but that wasn't part of the directions so I decided agsinst that
+- I wanted to prompt the user to set the max number of floors, but that wasn't part of the directions so I decided against that
 - I had initially wanted to sort the flooors so that we would travel to them in order, much like how a real elevator would function, but when I determined how total travel time was calculated I realized this was incorrect and was further confirmed by Monica via email
--- src/elevator.js
+++ src/elevator.js
@@ -1,27 +1,101 @@
-// const maxFloor= parseInt(prompt("What is the maximum floor in the building?"));
-// const floorList = prompt("Please enter the list of floors you want to visit, separated by commas:")
-//   .split(",")
-//   .map(floor => parseInt(floor));
-
-
-
-const currentFloor = 12;
-const destinationFloors = [2, 9, 1, 32];
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+// set base and max floors as constants
+// that way we only need to change them in one place, and the rest of the code will still work
+const baseFloor = 0;
+const maxNumberOfFloors = 163;
 const singleFloorTravelTimeInSeconds = 10;
 
-const differences = destinationFloors.reduce((acc, curr, index, arr) => {
-  if (index === 0) {
-    acc.push(Math.abs(currentFloor - arr[index]));
-  } else if (index > 0) {
-    acc.push(Math.abs(curr - arr[index - 1]));
+// function to prompt the user to enter their current floor
+function askCurrentFloor() {
+  return new Promise((resolve) => {
+    rl.question(
+      `Please enter current floor (${baseFloor}-${maxNumberOfFloors}): `,
+      (answer) => {
+        // convert string input to an integer
+        const currentFloor = parseInt(answer);
+
+        // data validation to determine whether the input is valid
+        // data validation is used to prevent the user from inputting invalid data that could cause the program to crash or produce incorrect results.
+        if (isNaN(currentFloor) || currentFloor < baseFloor || currentFloor > maxNumberOfFloors) {
+          console.log("Invalid input, please try again.");
+          resolve(askCurrentFloor());
+        } else {
+          resolve(currentFloor);
+        }
+      }
+    );
+  });
+}
+
+// function to prompt user to enter list of floors to travel to
+function askDestinationFloors() {
+  return new Promise((resolve) => {
+    rl.question(
+      "Please enter destination floors separated by commas: ",
+      (answer) => {
+        // convert input into an array
+        const floorsArray = answer
+          .split(",")
+          .map((floor) => parseInt(floor.trim()));
+
+        // data validation to determine whether the input is valid
+        // data validation is used to prevent the user from inputting invalid data that could cause the program to crash or produce incorrect results.
+        if (
+          floorsArray.some(
+            (floor) =>
+              isNaN(floor) || floor < baseFloor || floor > maxNumberOfFloors
+          )
+        ) {
+          console.log("Invalid input, please try again.");
+          resolve(askDestinationFloors());
+        } else {
+          resolve(floorsArray);
+        }
+      }
+    );
+  });
+}
+
+async function main() {
+  const currentFloor = await askCurrentFloor();
+  const destinationFloors = await askDestinationFloors();
+
+  // calculate the differences in each value of the list of floors. 
+  // This determines how many floors were passed during travel time. 
+  // We push these values into an array which we will later use
+  const differences = destinationFloors.reduce((acc, curr, index, arr) => {
+    // if it's the first value in the list, we subtract the current floor with the intial floor in the list
+    if (index === 0) {
+      acc.push(Math.abs(currentFloor - arr[index]));
+    } 
+    // otherwise we subtract current list value starting at index 1, but the previous value
+    else if (index > 0) {
+      acc.push(Math.abs(curr - arr[index - 1]));
+    }
+    // we push all of these into our initial value in the reduce function, which is an empty array
+    return acc;
+  }, []);
+
+  // we use the reduce function to add all the values in the array calculated from const differences
+  // this number represents the number of floors visited
+  const numberOfFloorsVisited = differences.reduce((acc, curr) => acc + curr, 0);
+
+  // multiply number of floors traveled by time it takes to travel to a single floor to calculate total travel time
+  const totalTravelTime = numberOfFloorsVisited * singleFloorTravelTimeInSeconds;
+
+  // if the initial floor is equal to the list of floors traveled to, then it is omitted from the list of floors traveled to because it is redundant
+  // otherwise, add the initial floor to the list of floors traveled to 
+  if (currentFloor != destinationFloors[0]) {
+    destinationFloors.unshift(currentFloor);
   }
-  return acc;
-}, []);
 
-const sumOfDifferences = differences.reduce((acc, curr) => acc + curr, 0);
-const totalTravelTime = sumOfDifferences * singleFloorTravelTimeInSeconds;
+  console.log("Total travel time:", totalTravelTime);
+  console.log("Floors visited in order:", destinationFloors);
+  rl.close();
+}
 
-destinationFloors.unshift(currentFloor);
-
-console.log("Total travel time:", totalTravelTime);
-console.log("Floors visited in order:", destinationFloors);
+main();
