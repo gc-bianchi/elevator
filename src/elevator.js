d86c7f3b9ce032a3e60189b3bc413beb6ae564b0
gc-bianchi <72824902+gc-bianchi@users.noreply.github.com>
2023-05-04 19:42:19 -0600

    prettier

--- src/elevator.js
+++ src/elevator.js
@@ -20,7 +20,11 @@ function askCurrentFloor() {
 
         // data validation to determine whether the input is valid
         // data validation is used to prevent the user from inputting invalid data that could cause the program to crash or produce incorrect results.
-        if (isNaN(currentFloor) || currentFloor < baseFloor || currentFloor > maxNumberOfFloors) {
+        if (
+          isNaN(currentFloor) ||
+          currentFloor < baseFloor ||
+          currentFloor > maxNumberOfFloors
+        ) {
           console.log("Invalid input, please try again.");
           resolve(askCurrentFloor());
         } else {
@@ -61,48 +65,63 @@ function askDestinationFloors() {
 }
 
 // function to remove duplicate values if they are adjacent to each other in the list
-// because there would be no movement between these values
+// because there would be no elevator movement between these values
 function removeAdjacentDuplicates(array) {
   return array.filter((val, index) => val !== array[index + 1]);
 }
 
+module.exports = {
+  askCurrentFloor,
+  removeAdjacentDuplicates,
+};
 
 async function main() {
   const currentFloor = await askCurrentFloor();
   const destinationFloors = await askDestinationFloors();
-  const destinationFloorsWithDuplicateValuesRemoved = removeAdjacentDuplicates(destinationFloors);
+  const destinationFloorsWithDuplicateValuesRemoved =
+    removeAdjacentDuplicates(destinationFloors);
 
-  // calculate the differences in each value of the list of floors. 
-  // This determines how many floors were passed during travel time. 
+  // calculate the differences in each value of the list of floors.
+  // This determines how many floors were passed during travel time.
   // We push these values into an array which we will later use
-  const differences = destinationFloorsWithDuplicateValuesRemoved.reduce((acc, curr, index, arr) => {
-    // if it's the first value in the list, we subtract the current floor with the intial floor in the list
-    if (index === 0) {
-      acc.push(Math.abs(currentFloor - arr[index]));
-    } 
-    // otherwise we subtract current list value starting at index 1, but the previous value
-    else if (index > 0) {
-      acc.push(Math.abs(curr - arr[index - 1]));
-    }
-    // we push all of these into our initial value in the reduce function, which is an empty array
-    return acc;
-  }, []);
+  const differences = destinationFloorsWithDuplicateValuesRemoved.reduce(
+    (acc, curr, index, arr) => {
+      // if it's the first value in the list, we subtract the current floor with the intial floor in the list
+      if (index === 0) {
+        acc.push(Math.abs(currentFloor - arr[index]));
+      }
+      // otherwise we subtract current list value starting at index 1, but the previous value
+      else if (index > 0) {
+        acc.push(Math.abs(curr - arr[index - 1]));
+      }
+      // we push all of these into our initial value in the reduce function, which is an empty array
+      return acc;
+    },
+    []
+  );
 
   // we use the reduce function to add all the values in the array calculated from const differences
   // this number represents the number of floors visited
-  const numberOfFloorsVisited = differences.reduce((acc, curr) => acc + curr, 0);
+  const numberOfFloorsVisited = differences.reduce(
+    (acc, curr) => acc + curr,
+    0
+  );
 
   // multiply number of floors traveled by time it takes to travel to a single floor to calculate total travel time
-  const totalTravelTime = numberOfFloorsVisited * singleFloorTravelTimeInSeconds;
+  const totalTravelTime =
+    numberOfFloorsVisited * singleFloorTravelTimeInSeconds;
 
   // if the initial floor is equal to the list of floors traveled to, then it is omitted from the list of floors traveled to because it is redundant
-  // otherwise, add the initial floor to the list of floors traveled to 
+  // otherwise, add the initial floor to the list of floors traveled to
   if (currentFloor != destinationFloorsWithDuplicateValuesRemoved[0]) {
     destinationFloorsWithDuplicateValuesRemoved.unshift(currentFloor);
   }
 
   console.log("Total travel time:", totalTravelTime);
-  console.log("Floors visited in order:", destinationFloorsWithDuplicateValuesRemoved);
+  console.log(
+    "Floors visited in order:",
+    destinationFloorsWithDuplicateValuesRemoved
+  );
   rl.close();
 }
 
